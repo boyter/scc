b55613b95aa09eafa735a1af05947c888344b7d0
Ben Boyter <ben@boyter.org>
2018-03-02 21:35:04 +1100

    Goroutine all the things

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"github.com/ryanuber/columnize"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"runtime"
+	"sync"
 )
 
 type FileJob struct {
@@ -20,71 +22,86 @@ var FileReadJobQueue = make(chan FileJob, runtime.NumCPU()*10)
 var FileProcessJobQueue = make(chan FileJob, runtime.NumCPU()*10)
 var FileSummaryJobQueue = make(chan FileJob, runtime.NumCPU()*10)
 
-func readFile(filepath string) []byte {
-	// TODO only read as deep into the file as we need
-	bytes, err := ioutil.ReadFile(filepath)
-
-	if err != nil {
-		fmt.Print(err)
-	}
-
-	return bytes
-}
-
 /// Get all the files that exist in the directory
-func walkDirectory(directory string) {
-	all, _ := ioutil.ReadDir(directory)
-	directories := []string{}
-
-	// Work out which directories and files we want to investigate
-	for _, f := range all {
-		if f.IsDir() {
-			directories = append(directories, f.Name())
-		} else {
-			FileReadJobQueue <- FileJob{Filename: f.Name(), Location: filepath.Join(directory, f.Name())}
+/// We do it this way rather than walk because we want
+/// to group files together potentially
+func walkDirectory(root string) {
+	var wg sync.WaitGroup
+	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
 		}
-	}
-
-	for _, newdirectory := range directories {
-		walkDirectory(filepath.Join(directory, newdirectory))
-	}
-}
-
-func fileProcessorWorker() {
-	for {
-		// Blocks till it gets something
-		res := <-FileProcessJobQueue
-
-		// Do some pointless work
-		count := 0
-		for _, i := range res.Content {
-			if i == 0 {
-				count++
-			}
+		if !info.Mode().IsRegular() {
+			return nil
 		}
+		wg.Add(1)
+		go func() { // HL
+			FileReadJobQueue <- FileJob{Location: path}
+			wg.Done()
+		}()
 
-		fmt.Println(res.Filename, res.Location, len(res.Content), count)
-		FileSummaryJobQueue <- FileJob{Filename: res.Filename, Location: res.Location, Count: int64(count)}
-	}
+		return nil
+	})
+	// Walk has returned, so all calls to wg.Add are done.  Start a
+	// goroutine to close c once all the sends are done.
+	go func() { // HL
+		wg.Wait()
+		close(FileReadJobQueue) // HL
+	}()
 }
 
 func fileReaderWorker() {
-	for {
-		// Blocks till it gets something
-		res := <-FileReadJobQueue
-		content := readFile(res.Location)
-		FileProcessJobQueue <- FileJob{Filename: res.Filename, Location: res.Location, Content: content}
+	var wg sync.WaitGroup
+	for res := range FileReadJobQueue {
+		wg.Add(1)
+		go func() {
+			content, _ := ioutil.ReadFile(res.Location)
+			FileProcessJobQueue <- FileJob{Filename: res.Filename, Location: res.Location, Content: content}
+			wg.Done()
+		}()
 	}
+
+	wg.Wait()
+	close(FileProcessJobQueue)
+}
+
+func fileProcessorWorker() {
+	var wg sync.WaitGroup
+	for res := range FileProcessJobQueue {
+		// Do some pointless work
+		wg.Add(1)
+		go func() {
+			count := 0
+			count2 := 0
+			for _, i := range res.Content {
+				if i == 0 {
+					count++
+				}
+
+				if i == 1 {
+					count2++
+				}
+			}
+
+			FileSummaryJobQueue <- FileJob{Filename: res.Filename, Location: res.Location, Count: int64(count)}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	close(FileSummaryJobQueue)
 }
 
 func main() {
-	for i := 0; i < runtime.NumCPU()+2; i++ {
-		go fileProcessorWorker()
-		go fileReaderWorker()
-	}
+	go fileReaderWorker()
+	go fileProcessorWorker()
 
 	walkDirectory("../")
 	fmt.Println("Finished running...")
+
+	for res := range FileSummaryJobQueue {
+		fmt.Println(res.Filename, res.Location, len(res.Content), res.Count)
+	}
 
 	// Once done lets print it all out
 	output := []string{
@@ -92,6 +109,5 @@ func main() {
 	}
 
 	result := columnize.SimpleFormat(output)
-
 	fmt.Println(result)
 }
