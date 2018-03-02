6eb0ff05321234fd02ba78815818bdf5102b2be8
Ben Boyter <ben@boyter.org>
2018-03-02 18:02:40 +1100

    Add in vendor dependancy

--- main.go
+++ main.go
@@ -2,43 +2,89 @@ package main
 
 import (
 	"fmt"
+	"github.com/ryanuber/columnize"
 	"io/ioutil"
 	"path/filepath"
+	"runtime"
 )
 
 type FileJob struct {
 	Location string
+	Content  []byte
 }
 
 // A buffered channel that we can send work requests on.
-var FileJobQueue = make(chan FileJob, 100)
+var FileReadJobQueue = make(chan FileJob, runtime.NumCPU()*10)
+var FileProcessJobQueue = make(chan FileJob, runtime.NumCPU()*10)
+
+func readFile(filepath string) []byte {
+	// TODO only read as deep into the file as we need
+	bytes, err := ioutil.ReadFile(filepath)
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	return bytes
+}
 
 /// Get all the files that exist in the directory
-func walkDirectory(directory string) []FileJob {
+func walkDirectory(directory string) {
 	all, _ := ioutil.ReadDir(directory)
-
 	directories := []string{}
-	files := []FileJob{}
 
 	// Work out which directories and files we want to investigate
 	for _, f := range all {
 		if f.IsDir() {
 			directories = append(directories, f.Name())
 		} else {
-			files = append(files, FileJob{Location: filepath.Join(directory, f.Name())})
-
-			FileJobQueue <- FileJob{Location: filepath.Join(directory, f.Name())}
+			FileReadJobQueue <- FileJob{Location: filepath.Join(directory, f.Name())}
 		}
 	}
 
 	for _, newdirectory := range directories {
-		files = append(files, walkDirectory(filepath.Join(directory, newdirectory))...)
+		walkDirectory(filepath.Join(directory, newdirectory))
 	}
+}
 
-	return files
+func fileProcessorWorker() {
+	for {
+		res := <-FileProcessJobQueue
+
+		count := 0
+		for _, i := range res.Content {
+			if i == 0 {
+				count++
+			}
+		}
+
+		fmt.Println(res.Location, len(res.Content), count)
+	}
+}
+
+func fileReaderWorker() {
+	for {
+		res := <-FileReadJobQueue
+		content := readFile(res.Location)
+		FileProcessJobQueue <- FileJob{Location: res.Location, Content: content}
+	}
 }
 
 func main() {
+	for i := 0; i < runtime.NumCPU()+2; i++ {
+		go fileProcessorWorker()
+		go fileReaderWorker()
+	}
+
 	fmt.Println("Main")
-	fmt.Println(walkDirectory("../"))
+	walkDirectory("../")
+
+	// Once done lets print it all out
+	output := []string{
+		"Directory | File | License | Confidence | Size",
+	}
+
+	result := columnize.SimpleFormat(output)
+
+	fmt.Println(result)
 }
