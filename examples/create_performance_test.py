7524dadc1237d8cfe727a16ddcad5b75abf4c5af
Ben Boyter <ben@boyter.org>
2018-09-17 10:14:42 +1000

    Modify test to be more realistic

--- examples/create_performance_test.py
+++ examples/create_performance_test.py
@@ -3,27 +3,156 @@
 import os
 import errno
 
-code = '''public class Test
-{
-    int j = 0;
-    public static void main(String[] args)
-    {
-        Foo f = new Foo();
-        f.bar();
-        f.bar();
-    }
-}
+code = '''package com.boyter.SpellingCorrector;
 
-class Foo
-{
-    public void bar()
-    {
-          System.out.println("FooBar"); //Not counted
-          System.out.println("this is a string")
-    }
-}
+import java.util.*;
+import java.util.stream.Stream;
 
-'''
+public class SpellingCorrector implements ISpellingCorrector {
+  private Map<String, Integer> dictionary = null;
+
+  public SpellingCorrector(int lruCount) {
+      this.dictionary = Collections.synchronizedMap(new LruCache<>(lruCount));
+  }
+
+  @Override
+  public void putWord(String word) {
+      word = word.toLowerCase();
+      if (dictionary.containsKey(word)) {
+          dictionary.put(word, (dictionary.get(word) + 1));
+      }
+      else {
+          dictionary.put(word, 1);
+      }
+  }
+
+  @Override
+  public String correct(String word) {
+      if (word == null || word.trim().isEmpty()) {
+          return word;
+      }
+
+      word = word.toLowerCase();
+
+      if (dictionary.containsKey(word)) {
+          return word;
+      }
+
+      Map<String, Integer> possibleMatches = new HashMap<>();
+
+      List<String> closeEdits = wordEdits(word);
+      for (String closeEdit: closeEdits) {
+          if (dictionary.containsKey(closeEdit)) {
+              possibleMatches.put(closeEdit, this.dictionary.get(closeEdit));
+          }
+      }
+
+      if (!possibleMatches.isEmpty()) {
+          Object[] matches = this.sortByValue(possibleMatches).keySet().toArray();
+
+          String bestMatch = "";
+          for(Object o: matches) {
+              if (o.toString().length() == word.length()) {
+                  bestMatch = o.toString();
+              }
+          }
+
+          if (!bestMatch.trim().isEmpty()) {
+              return bestMatch;
+          }
+
+          return matches[matches.length - 1].toString();
+      }
+
+      List<String> furtherEdits = new ArrayList<>();
+      for(String closeEdit: closeEdits) {
+          furtherEdits.addAll(this.wordEdits(closeEdit));
+      }
+
+      for (String futherEdit: furtherEdits) {
+          if (dictionary.containsKey(futherEdit)) {
+              possibleMatches.put(futherEdit, this.dictionary.get(futherEdit));
+          }
+      }
+
+      if (!possibleMatches.isEmpty()) {
+          Object[] matches = this.sortByValue(possibleMatches).keySet().toArray();
+
+          String bestMatch = "";
+          for(Object o: matches) {
+              if (o.toString().length() == word.length()) {
+                  bestMatch = o.toString();
+              }
+          }
+
+          if (!bestMatch.trim().isEmpty()) {
+              return bestMatch;
+          }
+
+          return matches[matches.length - 1].toString();
+      }
+
+      return word;
+  }
+
+  @Override
+  public boolean containsWord(String word) {
+      if (dictionary.containsKey(word)) {
+          return true;
+      }
+
+      return false;
+  }
+
+  private List<String> wordEdits(String word) {
+      List<String> closeWords = new ArrayList<String>();
+
+      for (int i = 1; i < word.length() + 1; i++) {
+          for (char character = 'a'; character <= 'z'; character++) {
+              StringBuilder sb = new StringBuilder(word);
+              sb.insert(i, character);
+              closeWords.add(sb.toString());
+          }
+      }
+
+      for (int i = 1; i < word.length(); i++) {
+          for (char character = 'a'; character <= 'z'; character++) {
+              StringBuilder sb = new StringBuilder(word);
+              sb.setCharAt(i, character);
+              closeWords.add(sb.toString());
+
+              sb = new StringBuilder(word);
+              sb.deleteCharAt(i);
+              closeWords.add(sb.toString());
+          }
+      }
+
+      return closeWords;
+  }
+
+  public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue( Map<K, V> map ) {
+      Map<K, V> result = new LinkedHashMap<>();
+      Stream<Map.Entry<K, V>> st = map.entrySet().stream();
+
+      st.sorted( Map.Entry.comparingByValue() ).forEachOrdered( e -> result.put(e.getKey(), e.getValue()) );
+
+      return result;
+  }
+
+  public class LruCache<A, B> extends LinkedHashMap<A, B> {
+      private final int maxEntries;
+
+      public LruCache(final int maxEntries) {
+          super(maxEntries + 1, 1.0f, true);
+          this.maxEntries = maxEntries;
+      }
+
+      @Override
+      protected boolean removeEldestEntry(final Map.Entry<A, B> eldest) {
+          return super.size() > maxEntries;
+      }
+  }
+}'''
 
 def make_sure_path_exists(path):
     try:
