416bfbec75032677d6d0f2b317e97e0e5d8f1d9d
Ryan Graham <r.m.graham@gmail.com>
2018-12-19 12:17:24 -0800

    Reduce regexp compilation overhead
    When using an exclusion pattern only compile it once at the start of a
    directory traversal instead of once per item in the walk.

--- processor/file.go
+++ processor/file.go
@@ -181,15 +181,15 @@ func walkDirectory(toWalk string, blackList []string, extensionLookup map[string
 	extension := ""
 	var filejobs []FileJob
 
+	var regex *regexp.Regexp
+	if Exclude != "" {
+		regex = regexp.MustCompile(Exclude)
+	}
+
 	godirwalk.Walk(toWalk, &godirwalk.Options{
 		// Unsorted is meant to make the walk faster and we need to sort after processing anyway
 		Unsorted: true,
 		Callback: func(root string, info *godirwalk.Dirent) error {
-
-			var regex *regexp.Regexp
-			if Exclude != "" {
-				regex = regexp.MustCompile(Exclude)
-			}
 
 			if Exclude != "" {
 				if regex.Match([]byte(info.Name())) {
