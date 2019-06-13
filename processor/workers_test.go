31828510851a0c1f640b04d467ab431e7efab11f
Ben Boyter <ben@boyter.org>
2019-06-13 18:28:34 +1000

    Improve coverage slightly

--- processor/workers_test.go
+++ processor/workers_test.go
@@ -13,6 +13,22 @@ func TestIsWhitespace(t *testing.T) {
 	}
 }
 
+func TestIsBinaryTrue(t *testing.T) {
+	DisableCheckBinary = false
+
+	if !isBinary(0 , 0) {
+		t.Errorf("Expected to be true")
+	}
+}
+
+func TestIsBinaryDisableCheck(t *testing.T) {
+	DisableCheckBinary = true
+
+	if isBinary(0 , 0) {
+		t.Errorf("Expected to be false")
+	}
+}
+
 func TestCountStatsLines(t *testing.T) {
 	Trace = false
 	Debug = false
