0f3ae99fa3da6429880bc76b1ee2f8e585404cf0
Ben Boyter <ben@boyter.org>
2019-08-12 01:24:51 +1000

    Update to have another go test

---
+++ examples/language/go.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// This is a comment
+/**
+	another comment
+**/
+
+// Msg function
+func Msg() {
+
+}
+
+func main() {
+	fmt.Println("hw")
+}
+
+// This is a comment
