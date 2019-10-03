0308ce2761c83c6f41456b3298bd26f03d443fed
Ben Boyter <ben@boyter.org>
2019-10-04 08:56:50 +1000

    Remove redundant code

--- processor/detector.go
+++ processor/detector.go
@@ -5,28 +5,6 @@ import (
 	"strings"
 )
 
-var sheBangMatches = map[string][]string{
-	"Perl":       {"perl"},
-	"PHP":        {"php"},
-	"Python":     {"python"},
-	"AWK":        {"awk", "gawk", "mawk"},
-	"C Shell":    {"csh", "tcsh"},
-	"D":          {"rdmd"},
-	"Erlang":     {"escript"},
-	"JavaScript": {"node"},
-	"Lisp":       {"sbcl"},
-	"Scheme":     {"racket"},
-	"Fish":       {"fish"},
-	"BASH":       {"bash"},
-	"Shell":      {"sh"},
-	"Ruby":       {"ruby"},
-	"Lua":        {"lua"},
-	"Korn Shell": {"ksh"},
-	"sed":        {"sed"},
-	"TCL":        {"tcl"},
-	"ZSH":        {"zsh"},
-}
-
 // Given some content attempt to determine if it has a #! that maps to a known language and return the language
 func DetectSheBang(content string) (string, error) {
 	ProcessConstants()
