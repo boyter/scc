4db33fad82d8ef5163ab3018f59b1428e3ebcebc
Ben Boyter <ben@boyter.org>
2020-08-27 08:56:19 +1000

    Fix COCOMO values https://github.com/boyter/scc/issues/191

--- processor/cocomo.go
+++ processor/cocomo.go
@@ -7,7 +7,7 @@ import (
 // EstimateCost calculates the cost in dollars applied using generic COCOMO weighted values based
 // on the average yearly wage
 func EstimateCost(effortApplied float64, averageWage int64) float64 {
-	return effortApplied * float64(averageWage/12) * float64(1.8)
+	return effortApplied * float64(averageWage/12) * float64(2.4)
 }
 
 // EstimateEffort calculate the effort applied using generic COCOMO weighted values
@@ -15,7 +15,7 @@ func EstimateEffort(sloc int64) float64 {
 	var eaf float64 = 1
 
 	// Numbers based on organic project, small team, good experience working with requirements
-	var effortApplied = float64(3.2) * math.Pow(float64(sloc)/1000, 1.05) * eaf
+	var effortApplied = float64(2.4) * math.Pow(float64(sloc)/1000, 1.05) * eaf
 	return effortApplied
 }
 
