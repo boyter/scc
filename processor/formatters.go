c3445c69b5c1be2794218b527893365b427edeb3
Ben Boyter <ben@boyter.org>
2021-12-16 16:08:51 +1100

    add error output for formatter

--- processor/formatters.go
+++ processor/formatters.go
@@ -646,9 +646,11 @@ func fileSummarizeMulti(input chan *FileJob) string {
 				str.WriteString(val)
 				str.WriteString("\n")
 			} else {
-				_ = ioutil.WriteFile(t[1], []byte(val), 0600)
+				err := ioutil.WriteFile(t[1], []byte(val), 0600)
+				if err != nil {
+					fmt.Println(fmt.Sprintf("%s unable to be written to for format %s", t[1], t[0]))
+				}
 			}
-
 		}
 	}
 
