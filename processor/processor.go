cf36893c1b7b8e79b25f4679366520736d369e30
Ben Boyter <ben@boyter.org>
2024-09-30 15:41:47 +1000

    Version 3.5.0 beta

--- processor/processor.go
+++ processor/processor.go
@@ -23,7 +23,7 @@ import (
 )
 
 // Version indicates the version of the application
-var Version = "3.4.0"
+var Version = "3.5.0 (beta)"
 
 // Flags set via the CLI which control how the output is displayed
 
