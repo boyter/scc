953f512579a6aa577ee68b488db8d996015eb759
Ben Boyter <ben@boyter.org>
2025-02-17 11:50:45 +1100

    Cut 3.5.0 release

--- processor/processor.go
+++ processor/processor.go
@@ -23,7 +23,7 @@ import (
 )
 
 // Version indicates the version of the application
-var Version = "3.5.0 (beta)"
+var Version = "3.5.0"
 
 // Flags set via the CLI which control how the output is displayed
 
