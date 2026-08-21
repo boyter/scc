eb336b3a248bef571bec0b1da0285006f53e17fd
apocelipes <seve3r@outlook.com>
2026-08-21 18:05:07 +0800

    feat: improve qt translation source detection

---
+++ examples/language/qt_translation_source.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP" sourcelanguage="en_US">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="./about.cpp" line="60"/>
+        <source>About Qt</source>
+        <translation>Qtについて</translation>
+    </message>
+    <message>
+        <location filename="./about.cpp" line="71"/>
+        <source>Copy</source>
+        <translation>コピー</translation>
+    </message>
+    <message>
+        <location filename="./about.cpp" line="85"/>
+        <source>Copy version information to the clipboard.</source>
+        <translation>バージョン情報をクリップボードにコピー。</translation>
+    </message>
+</context>
+</TS>
